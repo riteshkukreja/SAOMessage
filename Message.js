8dee1869c7e8af07722d72911d877bdc2e45e9d6
Ritesh <ritesh@mettl.com>
2017-02-14 13:00:28 +0530

    Initial Commit

---
+++ Message.js
@@ -0,0 +1,13 @@
+var Message = function(title, content, success, failure) {
+	this.title = title;
+	this.content = content;
+	this.success = success;
+	this.failure = failure;
+
+	this.dom = null;
+
+	this.show = function() {
+		var Holder = document.createElement("div");
+		
+	};
+}
